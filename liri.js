0f95db49404849a21414fb3cb77a6213ac3a6b49
Ky hong Le <kyhongle08@gmail.com>
2019-03-26 20:15:11 -0500

    initial files

---
+++ liri.js
@@ -0,0 +1,21 @@
+// pseudocode
+// install all the given packages
+
+// require ("dotenv").config() - should be at the top all the time
+// require keys.js
+// var keys=require("./keys.js")
+
+// var spotify = new Spotify(keys.spotify)
+
+// user command choice
+// process.argv or inquirer
+
+// check for user command
+// if or switch
+// cond(concert-this)
+// create a function: function concert()
+
+// concert()
+// have to use axios
+// use this url: "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+// display name, location, date
